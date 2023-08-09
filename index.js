b27f79218652f5e163813dbaa7cb3d49e6605c04
hoanrosi <hoanroi@gmail.com>
2023-08-09 23:30:58 +0700

    not save multer

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 const router = require("./route/index");
 const { kStringMaxLength } = require("buffer");
 const url = process.env.URL;
-const upload = multer({ dest: "uploads/" });
+const upload = multer();
 const connectDB = async () => {
   try {
     await mongoose.connect(url);
