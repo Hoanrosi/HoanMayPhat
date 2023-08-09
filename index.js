e97993967f081c8a837c38e08eb85afa190cbccb
hoanrosi <hoanroi@gmail.com>
2023-08-09 23:29:19 +0700

    fix bug

--- index.js
+++ index.js
@@ -26,57 +26,57 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api", router);
 // Define mongoose schema and model
-// const DataModel = mongoose.model("Data", {
-//   UPS: [
-//     {
-//       "Tên Bình": String,
-//       "Ngày đo kiểm": String,
-//       "Nội trở đo kiểm": Number,
-//       "Điện áp đo kiểm": Number,
-//       "Nhiệt độ đo kiểm": Number,
-//       "Hãng UPS": String,
-//       "Tổ ắc quy": String,
-//       "Nhà sản xuất": String,
-//       "Xuất xứ": String,
-//       "Model ắc quy": String,
-//       "Thời gian lắp đặt": String,
-//       "Số năm sử dụng": Number,
-//       "Kích thước (mm)": String,
-//     },
-//   ],
-// });
+const DataModel = mongoose.model("Data", {
+  UPS: [
+    {
+      "Tên Bình": String,
+      "Ngày đo kiểm": String,
+      "Nội trở đo kiểm": Number,
+      "Điện áp đo kiểm": Number,
+      "Nhiệt độ đo kiểm": Number,
+      "Hãng UPS": String,
+      "Tổ ắc quy": String,
+      "Nhà sản xuất": String,
+      "Xuất xứ": String,
+      "Model ắc quy": String,
+      "Thời gian lắp đặt": String,
+      "Số năm sử dụng": Number,
+      "Kích thước (mm)": String,
+    },
+  ],
+});
 
-// app.post("/upload", upload.single("excel"), async (req, res) => {
-//   try {
-//     const filePath = req.file.path;
-//     const workbook = xlsx.readFile(filePath);
+app.post("/upload", upload.single("excel"), async (req, res) => {
+  try {
+    const filePath = req.file.path;
+    const workbook = xlsx.readFile(filePath);
 
-//     const upSheets = ["UPS1", "UPS 2", "UPS 3", "UPS 4"];
+    const upSheets = ["UPS1", "UPS 2", "UPS 3", "UPS 4"];
 
-//     const allSheetData = {};
+    const allSheetData = {};
 
-//     upSheets.forEach((sheetName) => {
-//       const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-//       const newData = new DataModel({
-//         UPS: sheetData,
-//       });
-//       newData.save();
-//     });
+    upSheets.forEach((sheetName) => {
+      const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+      const newData = new DataModel({
+        UPS: sheetData,
+      });
+      newData.save();
+    });
 
-//     res.status(200).json({ message: "Data imported successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: "An error occurred during data import" });
-//   }
-// });
+    res.status(200).json({ message: "Data imported successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred during data import" });
+  }
+});
 
-// app.get("/get-all-data", async (req, res) => {
-//   try {
-//     const allData = await DataModel.find();
-//     res.status(200).json(allData);
-//   } catch (error) {
-//     res.status(500).json({ error: "An error occurred while fetching data" });
-//   }
-// });
+app.get("/get-all-data", async (req, res) => {
+  try {
+    const allData = await DataModel.find();
+    res.status(200).json(allData);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!?");
